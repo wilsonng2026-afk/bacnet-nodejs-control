9ea068a33a2447d9f56693bffb26ff20ad0f2857
wilsonng2026-afk <wilsonng2026@gmail.com>
2026-09-25 15:21:53 +0800

    Add files via upload

---
+++ package.json.js
@@ -0,0 +1,12 @@
+{
+  "name": "bacnet-nodejs-control",
+  "version": "1.0.0",
+  "description": "Node.js BACnet controller for monitoring binary inputs and controlling binary outputs.",
+  "main": "index.js",
+  "scripts": {
+    "start": "node index.js"
+  },
+  "dependencies": {
+    "bacstack": "^0.1.0"
+  }
+}
